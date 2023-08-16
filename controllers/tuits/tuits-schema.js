e24710c53fa9cf0b3ae3380c3495d6abaa0d372a
SrikanthBonkuri <srikanthbonkuri2730@gmail.com>
2023-08-16 01:48:56 -0500

    a6 tuits

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,12 @@ const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
   liked: Boolean,
+  replies: Number,
+  retuits: Number,
+  topic: String,
+  userName: String,
+  handle: String,
+  time: String,
+  image: String
 }, {collection: 'tuits'});
 export default schema;
